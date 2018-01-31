a4a5d61e2621c1a2ec5aef45cc120c79c952cc03
Victor Petukhov <i@victor.am>
2018-02-01 00:59:48 +0300

    TimeLogger refactored

--- anomaly_selection.py
+++ anomaly_selection.py
@@ -8,35 +8,34 @@ from lib.helpers.TimeLogger import TimeLogger
 
 
 def binary_read(differences_file):
-    differences_read_logger = TimeLogger()
+    differences_read_logger = TimeLogger(task_name='Differences read')
     with open(differences_file, 'rb') as f:
         buffer = f.read(4)
         differences = []
         chunk_counter = 0
         log_write_per_chunk_number = 10000000
-        chunks_time_logger = TimeLogger()
+        chunks_time_logger = TimeLogger(task_name='Read and unpack %d chunks' % chunk_counter)
         while buffer:
             differences.append(struct.unpack('=f', buffer))
             buffer = f.read(4)
             if (chunk_counter + 1) % log_write_per_chunk_number == 0:
-                print(
-                    str(chunk_counter + 1) + ' chunks is read and unpacked. Time: ' + str(chunks_time_logger.finish()))
-                chunks_time_logger = TimeLogger()
+                chunks_time_logger.finish()
+                chunks_time_logger = TimeLogger(task_name='Read and unpack %d chunks' % chunk_counter)
             chunk_counter += 1
-        print(str(chunk_counter) + ' chunks is read and unpacked. Time: ' + str(chunks_time_logger.finish()))
-    print('Differences read finished. Time: ' + str(differences_read_logger.finish()))
+        chunks_time_logger.finish()
+    differences_read_logger.finish()
 
-    transformation_logger = TimeLogger()
+    transformation_logger = TimeLogger(task_name='Differences transformation')
     differences = np.array(differences)
     features_number = int(differences[-1])
     differences = differences[:-1].reshape(int(len(differences) / features_number), features_number)
-    print('Differences transformation finished. Time: ' + str(transformation_logger.finish()))
+    transformation_logger.finish()
 
     return differences
 
 
 def ascii_read(differences_file):
-    differences_read_logger = TimeLogger()
+    differences_read_logger = TimeLogger(task_name='Differences read')
     with open(differences_file) as f:
         differences = json.loads(f.read())
 
@@ -46,35 +45,35 @@ def ascii_read(differences_file):
             difference_indexes.append(difference[0])
             difference_values.append(difference[1])
 
-    print('Differences read finished. Time: ' + str(differences_read_logger.finish()))
+    differences_read_logger.finish()
 
     return difference_indexes, difference_values
 
 
 def dbscan_anomaly_selection(differences):
-    dbscan_time_logger = TimeLogger()
+    dbscan_time_logger = TimeLogger('DBSCAN its work')
     labels = DBSCAN(eps=3, min_samples=5, metric='euclidean').fit_predict(differences)
     anomaly_indexes = [i for i, x in enumerate(labels) if x == -1]
-    print('DBSCAN finished its work. Time: ' + str(dbscan_time_logger.finish()))
+    dbscan_time_logger.finish(full_finish=True)
 
     return anomaly_indexes
 
 
 def three_sigma_anomaly_selection(differences):
-    three_sigma_time_logger = TimeLogger()
+    three_sigma_time_logger = TimeLogger('3-sigma anomaly selection')
 
     difference_indexes, difference_values = differences
     mean = np.mean(difference_values)
     std_deviation = np.std(difference_values)
-    left_bound_3_sigma = mean - 5 * std_deviation
-    right_bound_3_sigma = mean + 5 * std_deviation
+    left_bound_3_sigma = mean - 3 * std_deviation
+    right_bound_3_sigma = mean + 3 * std_deviation
 
     anomalies = []
     for i, x in enumerate(difference_values):
         if x < left_bound_3_sigma or x > right_bound_3_sigma:
             anomalies.append((difference_indexes[i], difference_values[i]))
 
-    print('3-sigma anomaly selection finished its work. Time: ' + str(three_sigma_time_logger.finish()))
+    three_sigma_time_logger.finish(full_finish=True)
 
     return anomalies
 
@@ -91,7 +90,7 @@ def anomaly_selection(files_map_file, anomalies_output_file, use_dbscan, differe
     else:
         anomalies = three_sigma_anomaly_selection(differences)
 
-    anomalies_write_time_logger = TimeLogger()
+    anomalies_write_time_logger = TimeLogger('Anomaly list write')
     with open(files_map_file) as files_map_file_descriptor:
         files_map = json.loads(files_map_file_descriptor.read())
         anomaly_files = []
@@ -105,6 +104,6 @@ def anomaly_selection(files_map_file, anomalies_output_file, use_dbscan, differe
         with open(anomalies_output_file, 'w') as anomalies_output_file_descriptor:
             anomalies_output_file_descriptor.write(json.dumps(anomaly_files))
 
-    print('Anomaly list written. Time: ' + str(anomalies_write_time_logger.finish()))
+    anomalies_write_time_logger.finish(full_finish=True)
 
     return len(anomaly_files)
--- autoencoding.py
+++ autoencoding.py
@@ -10,22 +10,21 @@ from lib.helpers.TimeLogger import TimeLogger
 
 
 def binary_write(differences, features_number, output_file):
-    chunking_time_logger = TimeLogger()
+    chunking_time_logger = TimeLogger(task_name='Chunking')
     differences = differences.flatten('F')
     differences = np.append(differences, features_number)
     differences = struct.pack('=%df' % differences.size, *differences)
 
     chunk_size = 10000000
     difference_chunks = funcy.chunks(chunk_size, differences)
-    print('Chunking finished. Time: ' + str(chunking_time_logger.finish()))
+    chunking_time_logger.finish()
 
     chunk_counter = 1
     for difference_chunk in difference_chunks:
         with open(output_file, 'ab') as f:
-            difference_chunk_time_logger = TimeLogger()
+            difference_chunk_time_logger = TimeLogger(task_name='Write difference %d-th chunk' % chunk_counter)
             f.write(difference_chunk)
-            print('Write difference chunk ' + str(chunk_counter) + ' is done. Time: ' +
-                  str(difference_chunk_time_logger.finish()))
+            difference_chunk_time_logger.finish()
             chunk_counter += 1
 
 
@@ -35,25 +34,25 @@ def ascii_write(differences, output_file):
 
 
 def autoencoding(dataset_file, split_percent, encoding_dim_percent, output_file=None, full_differences=None):
-    time_logger = TimeLogger()
+    time_logger = TimeLogger(task_name='Dataset loading')
     data = DatasetLoader(dataset_file).load(split_percent=split_percent)
     (_, _, features_number) = data
     encoding_dim = math.ceil(features_number * encoding_dim_percent)
-    print('Dataset loaded. Time: ' + str(time_logger.finish()))
+    time_logger.finish(full_finish=True)
 
-    time_logger = TimeLogger()
+    time_logger = TimeLogger(task_name='Autoencoder fit')
     autoencoder = Autoencoder(features_number, encoding_dim, data)
     autoencoder.print_model_summary()
     autoencoder.fit()
-    print('Autoencoder fit finished. Time: ' + str(time_logger.finish()))
+    time_logger.finish(full_finish=True)
 
-    time_logger = TimeLogger()
+    time_logger = TimeLogger(task_name='Autoencoder predict')
     autoencoder.predict()
-    print('Autoencoder predict finished. Time: ' + str(time_logger.finish()))
+    time_logger.finish(full_finish=True)
 
-    time_logger = TimeLogger()
+    time_logger = TimeLogger(task_name='Calculate differences')
     differences = autoencoder.calc_differences(full_differences)
-    print('Calculate differences finished. Time: ' + str(time_logger.finish()))
+    time_logger.finish(full_finish=True)
 
     if not full_differences:
         differences = sorted(enumerate(differences), key=lambda tup: tup[1], reverse=True)
@@ -61,11 +60,11 @@ def autoencoding(dataset_file, split_percent, encoding_dim_percent, output_file=
     if not output_file:
         return differences
 
-    time_logger = TimeLogger()
+    time_logger = TimeLogger(task_name='Write differences')
 
     if full_differences:
         binary_write(differences, features_number, output_file)
     else:
         ascii_write(differences, output_file)
 
-    print('Write differences finished. Time: ' + str(time_logger.finish()))
+    time_logger.finish(full_finish=True)
